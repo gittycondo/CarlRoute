6aa5a9a76c603df9a2b5fd5e77dc02e139232fad
NoName <19268069+gittycondo@users.noreply.github.com>
2022-12-08 09:13:51 -0600

    Create points.swift

---
+++ points.swift
@@ -0,0 +1,8 @@
+// points.swift uses a graph data structure called "points" to connect to one another.
+// These points are linked like a graph, but include information of their coordinates
+
+Struct Point {
+  var x: Double
+  var y: Double
+  var point: Point
+}
