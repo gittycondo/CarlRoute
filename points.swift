0f11be455e41e5a9c5dff90519c24c817fbe3e36
NoName <19268069+gittycondo@users.noreply.github.com>
2022-12-09 15:07:53 -0600

    Delete points.swift

--- points.swift
+++
@@ -1,8 +0,0 @@
-// points.swift uses a graph data structure called "points" to connect to one another.
-// These points are linked like a graph, but include information of their coordinates
-
-Struct Point {
-  var x: Double
-  var y: Double
-  var point: Point
-}
